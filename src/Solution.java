8a8c34276bd955ab338d098892b60d8387ce4bbe
Ansar Ismagilov <ansar89kab@bk.ru>
2023-02-05 17:12:41 +0400

    EX 6 in the process

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,34 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    public boolean arePalindromes() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("Root/src/strings.txt"));
+    public void solution() throws IOException {
+        Scanner scanner = new Scanner(new FileInputStream("src/data2.txt"));
+        Writer writer = new FileWriter("src/output.txt");
 
-        String phrase = "";
-        boolean result = true;
-        List<Character> palindrome = new ArrayList<>();
+        int midStr;
+        int dfr = scanner.nextInt();
+        int allChars = 0;
 
-        while (scanner.hasNextLine()) {
-            phrase = scanner.nextLine().toLowerCase().replace(" ", "");
-            for (int i = phrase.length()-1; i >= 0; i--) {
-                palindrome.add(phrase.charAt(i));
-            }
-            int index = 0;
-            for (Character items : palindrome)
-                if (!items.equals(phrase.charAt(index++))) {
-                    result = false;
-                    break;
-                }
-            if (!result) break;
-            palindrome.clear();
+        //putting all strings to list
+        List<String> phrases = new ArrayList<>();
+        while (scanner.hasNextLine()){
+            phrases.add(scanner.nextLine());
         }
-        return result;
+        phrases.remove(0);
+        //count midStr
+        for (String s : phrases)
+            allChars += s.length();
+        midStr = allChars/ phrases.size();// midStr +-difference
+
+        writer.write(midStr + "\n");
+
+        for(String s:phrases) {
+            int sL = s.length();
+            if ((midStr - dfr) <= sL && sL <= (midStr + dfr)) writer.write(s + "\n");
+        }
+        writer.close();
     }
 }
