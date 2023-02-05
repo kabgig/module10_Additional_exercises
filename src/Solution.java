2d099a0e852107efebe24d93b6d540f303303649
Ansar Ismagilov <ansar89kab@bk.ru>
2023-02-05 17:42:41 +0400

    EX 7 DONE

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,28 @@
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    public void solution() throws IOException {
-        Scanner scanner = new Scanner(new FileInputStream("src/data2.txt"));
-        Writer writer = new FileWriter("src/output.txt");
+    public void dance() throws IOException {
+        Scanner scanner = new Scanner(new FileInputStream("src/dance.txt"));
+        Writer writer = new FileWriter("src/rhythm.txt");
+        int rhythm = Integer.parseInt(scanner.nextLine());
+        List<Integer> dances = new ArrayList<>();
 
-        int midStr;
-        int dfr = scanner.nextInt();
-        int allChars = 0;
-
-        //putting all strings to list
-        List<String> phrases = new ArrayList<>();
         while (scanner.hasNextLine()){
-            phrases.add(scanner.nextLine());
+            dances.add(Integer.parseInt(scanner.nextLine()));
         }
-        phrases.remove(0);
-        //count midStr
-        for (String s : phrases)
-            allChars += s.length();
-        midStr = allChars/ phrases.size();// midStr +-difference
-
-        writer.write(midStr + "\n");
-
-        for(String s:phrases) {
-            int sL = s.length();
-            if ((midStr - dfr) <= sL && sL <= (midStr + dfr)) writer.write(s + "\n");
+        String result =  "";
+        for (int d : dances){
+            int cyclesNumber = d/rhythm;
+            int restSteps = d%rhythm;
+            result += cyclesNumber + " " + restSteps + "\n";
         }
+        writer.write(result.trim());
         writer.close();
     }
 }
