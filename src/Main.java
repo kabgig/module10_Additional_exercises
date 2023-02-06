6fb1375585a9d8f5dcc3ce524acc5ee8c9b8396e
Ansar Ismagilov <ansar89kab@bk.ru>
2023-02-06 17:02:25 +0400

    EX 10 DONE

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args)
             throws IOException {
         var sol = new Solution();
-        sol.solution(" Central", 2016, 2017);
+        System.out.println(sol.findSquare());
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,25 @@
 import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Solution {
-    public void solution(String district, int fromYear, int toYear) throws IOException {
-        FileWriter fr = new FileWriter("src/data.txt");
-        Scanner sc = new Scanner(new FileInputStream("src/crop-volumes.txt"));
-        int firstYearIndex = 0, secondYearIndex = 0;
-        String[]data = sc.nextLine().split(";");
-        for (int i = 2; i < data.length; i++) {
-            if (data[i].equals(String.valueOf(fromYear))) firstYearIndex = i;
-            if (data[i].equals(String.valueOf(toYear))) secondYearIndex = i;
+    public double findSquare() throws FileNotFoundException {
+        Scanner scanner = new Scanner(
+                new FileInputStream("src/points.txt"));
+        String[] prevPoint = scanner.nextLine().split(" ");
+        double square = 0;
+        while (scanner.hasNextLine()) {
+            String[] nextPoint = scanner.nextLine().split(" ");
+            int prevX = Integer.parseInt(prevPoint[0]);
+            int prevY = Integer.parseInt(prevPoint[1]);
+            int nextX = Integer.parseInt(nextPoint[0]);
+            int nextY = Integer.parseInt(nextPoint[1]);
+            double a = prevY;
+            double b = nextY;
+            double h = nextX - prevX;
+            square += (a + b) / 2 + h;
+            prevPoint = nextPoint;
         }
-        String result = "Part;"+ fromYear + ";" + toYear + "\n";
-        while (sc.hasNextLine()){
-            String[] data2 = sc.nextLine().split(";");
-            double percCng = Double.parseDouble(data2[secondYearIndex]) -
-                    Double.parseDouble(data2[firstYearIndex]);
-            if (percCng > 4 && data2[1].equals(district)) result +=
-                    data2[0] + ";" +
-                    Double.parseDouble(data2[firstYearIndex]) + ";" +
-                            Double.parseDouble(data2[secondYearIndex]) + "\n";
-        }
-        fr.write(result.trim());
-        fr.close();
+        return square;
     }
 }
