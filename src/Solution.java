e9953c7c0f27ee813ccce4e7ba0d3bee54b8c445
Ansar Ismagilov <ansar89kab@bk.ru>
2023-02-08 17:34:56 +0400

    EX 15 in the process

--- src/Solution.java
+++ src/Solution.java
@@ -7,11 +7,22 @@ import java.util.Scanner;
 public class Solution {
     public void solution() throws FileNotFoundException {
         Scanner scanner = new Scanner(new FileInputStream("src/trees.txt"));
-
-        int[] trees = Arrays.stream(Arrays.stream(scanner
-                .nextLine()
-                .split(" ")
+        int result = 0;
+        int[] trees = Arrays.stream(scanner.nextLine()
+                        .split(" "))
                 .mapToInt(Integer::parseInt)
-                .toArray());
+                .toArray();
+        if (trees.length == 1) {
+            System.out.println(trees[0]);
+            return;
+        }
+        if (trees.length < 4) {
+            for (int i = 1; i < trees.length; i++) {
+                result = Math.max(trees[i - 1], trees[i]);
+            }
+            System.out.println(result);
+            return;
+        }
+
     }
 }
