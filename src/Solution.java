272eb58c6bc763c621beac9ec48c4722fad002e5
Ansar Ismagilov <ansar89kab@bk.ru>
2023-02-08 14:17:56 +0400

    EX 14 in the process

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,51 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-    public String translate() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("src/message.txt"));
-        String result = "";
-        while (scanner.hasNextLine()) {
-            String phrase = scanner.nextLine();
-            for (int i = 0; i < phrase.length(); i++) {
-                char c = phrase.charAt(i);
-                if (Character.isLetter(c)) {
-                    int chr = c;
-                    String biStr = Integer.toBinaryString(chr);
-                    result += biStr;
-                } else {
-                    result += String.valueOf(c);
-                }
+    public int findMaxQuantity() throws IOException {
+        Scanner scanner = new Scanner(new FileInputStream("src/forest.txt"));
+        Path path = Paths.get("src/forest.txt");
+        int numLines = (int)Files.lines(path).count();
+
+        int totalMushrooms = 0;
+        int index = 0;
+        int[] currentArray = Arrays
+                .stream(scanner.nextLine().split(";"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        numLines--;
+        totalMushrooms += currentArray[0];
+
+        int[] nextArray = Arrays
+                .stream(scanner.nextLine().split(";"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        numLines--;
+
+        while (numLines >0 ) {
+
+            if (index < currentArray.length - 1 && currentArray[index + 1] >= nextArray[index]) {
+                totalMushrooms += currentArray[index + 1];
+                index++;
+            } else {
+                totalMushrooms += nextArray[index];
+                currentArray = nextArray;
+                nextArray = Arrays
+                        .stream(scanner.nextLine().split(";"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+                numLines--;
+
             }
-            result += "\n";
         }
-        return result.trim();
+
+        while (currentArray.length > index) totalMushrooms += currentArray[index++];
+        return totalMushrooms;
     }
 }
